205a94b25a09d7a8abf9bcab4a9077a86f0e6a6d
raafpand <raafipandhika@gmail.com>
2025-05-27 09:12:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,8 @@ st.write(
     "Nafathan mendukung pembubaran band De Golden karena menilai lirik dan penampilan mereka tidak sesuai dengan nilai moral dan budaya lokal. Meski dikritik sebagai tindakan anti-ekspresi, ia yakin langkah ini perlu untuk menjaga etika di ruang publik"
 )
 st.image("IMG_4612.png", width=600)
+
+st.write(
+    "Selain itu Shandi mendukung pembubaran de golden dikarenakan beliau di keluarkan secara tidak hormat dari de golden"
+)
+st.image("
