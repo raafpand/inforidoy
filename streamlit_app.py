d092d48076d0fd728446445024fa3fa171608618
raafpand <raafipandhika@gmail.com>
2025-05-27 09:04:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Profil Nafatha Pendukung Aksi Pembubaran De Goden")
+st.title("Profil Nafathan Pendukung Aksi Pembubaran De Golden")
 st.write(
     "Nafathan mendukung pembubaran band De Golden karena menilai lirik dan penampilan mereka tidak sesuai dengan nilai moral dan budaya lokal. Meski dikritik sebagai tindakan anti-ekspresi, ia yakin langkah ini perlu untuk menjaga etika di ruang publik"
 )
